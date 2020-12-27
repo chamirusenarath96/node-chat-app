09b22712f96302a5e4d31f5b8ce5724a0eb3d859
Chamiru Senarath <chamisenarath@gmail.com>
2020-12-28 01:52:04 +0530

    Greet new User and alert the Others

--- server/servert.js
+++ server/servert.js
@@ -14,20 +14,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New User Conneted");
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat App',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
 
-    io.emit('newMessage', {
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
-    
+    });
+
   });
 
   socket.on('disconnect', () => {
     console.log("User Disconnected");
-  })
+  });
 
 });
 
